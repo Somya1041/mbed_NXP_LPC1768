51e0ef62c25d0ecf857cce4e2afe305f52d14ffe
Somya1041 <onlyskr1710@gmail.com>
2024-12-19 19:55:25 +0530

    uploaded all led blink file

---
+++ LED/all_led_blink.c
@@ -0,0 +1,25 @@
+# include <LPC17XX.h>
+
+#define ALL_LED (0xFF << 19)
+
+void delay_ms(uint32_t millis);
+
+int main(){
+	LPC_GPIO1 -> FIODIR |= ALL_LED;
+	LPC_GPIO1 -> FIOCLR = ALL_LED;
+
+	while(1){ 
+		LPC_GPIO1 -> FIOSET = ALL_LED;
+		delay_ms(500);
+
+		LPC_GPIO1 -> FIOCLR = ALL_LED;
+		delay_ms(500);
+	}
+}
+
+void delay_ms(uint32_t millis){
+	uint32_t i, j;
+	for (i = 0; i < millis; i++){
+		for (j = 0; j < 1250; i++){}
+	}
+}
