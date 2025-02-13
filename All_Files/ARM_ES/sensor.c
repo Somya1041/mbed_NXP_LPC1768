9af8783e97a0adde5e6a6aa2029d8079139c70e9
Somya1041 <onlyskr1710@gmail.com>
2025-02-13 18:01:14 +0530

    removing folder

--- All_Files/ARM_ES/sensor.c
+++
@@ -1,9 +0,0 @@
-#include "sensor.h"
-
-void sensor_init(void) {
-    LPC_GPIO2->FIODIR &= ~(1 << 10); // Set P2.10 as input (sensor pin)
-}
-
-uint8_t is_vehicle_detected(void) {
-    return !(LPC_GPIO2->FIOPIN & (1 << 10)); // Active low sensor
-}
